8d5fb090b79223bab8ba54542fb261ba984eb9cd
Ana <anarodriguez.p@hotmail.es>
2023-08-28 16:04:08 +0200

    first commit

---
+++ src/main/java/com/moviesnight/MoviesNight/MoviesNightApplication.java
@@ -0,0 +1,13 @@
+package com.moviesnight.MoviesNight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesNightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesNightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moviesnight/MoviesNight/MoviesNightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviesnight.MoviesNight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesNightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
