c9b59b42a269715b2db9ea450691a0b8629b3831
Ana <anarodriguez.p@hotmail.es>
2023-08-28 16:43:22 +0200

    routes added

--- src/main/frontend/src/app/app-routing.module.ts
+++ src/main/frontend/src/app/app-routing.module.ts
@@ -2,19 +2,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormloginComponent } from './Inscriptions/formlogin/formlogin.component';
 import { FormregisterComponent } from './Inscriptions/formregister/formregister.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { PlayComponent } from './views/play/play.component';
+import { FavoritesComponent } from './views/favorites/favorites.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { LiloystitchComponent } from './views/liloystitch/liloystitch.component';
+import { BarbieComponent } from './views/barbie/barbie.component';
+import { IntimeComponent } from './views/intime/intime.component';
+import { FreeguyComponent } from './views/freeguy/freeguy.component';
+import { DungeonsandragonsComponent } from './views/dungeonsandragons/dungeonsandragons.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'/login', pathMatch:'full'},
-  // {path: 'dashboard', component:DashboardComponent},
+  {path: 'dashboard', component:DashboardComponent},
   {path: 'login', component:FormloginComponent},
   {path: 'register', component:FormregisterComponent},
-  // {path: 'play', component:PlayComponent},
-  // {path: 'favorites', component:FavoritesComponent},
-  // {path: 'contact', component:ContactComponent},
-  // {path: 'liloystitch', component:LiloystitchComponent},
-  // {path: 'barbie', component:BarbieComponent},
-  // {path: 'intime', component: IntimeComponent},
-  // {path: 'freeguy', component:FreeguyComponent}
+  {path: 'play', component:PlayComponent},
+  {path: 'favorites', component:FavoritesComponent},
+  {path: 'contact', component:ContactComponent},
+  {path: 'liloystitch', component:LiloystitchComponent},
+  {path: 'barbie', component:BarbieComponent},
+  {path: 'intime', component: IntimeComponent},
+  {path: 'freeguy', component:FreeguyComponent},
+  {path: 'freeguy', component:DungeonsandragonsComponent},
 ];
 
 @NgModule({
