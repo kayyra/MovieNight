d471f9ef6731522a94dfa0f895f306db3446c8bb
Ana <anarodriguez.p@hotmail.es>
2023-08-29 11:52:24 +0200

    added play an cards component

--- src/main/frontend/src/app/views/play/play.component.ts
+++ src/main/frontend/src/app/views/play/play.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./play.component.scss']
 })
 export class PlayComponent {
-
+  cards = [
+    {title: 'Lilo & Stitch', movieCover: 'assets/img/lilo&stitch.jpg'},
+    {title: 'Freeguy',movieCover: 'assets/img/freeguy.jpg'},
+    {title: 'Barbie', movieCover: 'assets/img/barbie.jpg'},
+    {title: 'Barbie', movieCover: 'assets/img/dungeons&dragons.jpg'}
+   ];
+  
+   shuffleCards(){
+     for(let i = this.cards.length - 1; i > 0; i--){
+      const j = Math.floor(Math.random() * (i + 1 ));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+     }
+   }
 }
