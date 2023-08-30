b7aa339a2f6542727532dc450c2768617e6878f4
Ana <anarodriguez.p@hotmail.es>
2023-08-30 16:32:07 +0200

    java backend

--- src/main/java/com/moviesnight/MoviesNight/Models/user.java
+++ src/main/java/com/moviesnight/MoviesNight/Models/user.java
@@ -1,5 +1,53 @@
 package com.moviesnight.MoviesNight.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "register_form")
 public class user {
+    
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String confirmpassword;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getConfirmpassword() {
+        return confirmpassword;
+    }
+    public void setConfirmpassword(String confirmpassword) {
+        this.confirmpassword = confirmpassword;
+    }
+    
+
     
 }
--- src/main/java/com/moviesnight/MoviesNight/MoviesNightApplication.java
+++
@@ -1,7 +0,0 @@
-package com.moviesnight.MoviesNight;
-
-public class MoviesNightApplication {
-
-	
-
-}
