c35200716d2ca94ca093ae7abac98e7835dd2de4
Ana <anarodriguez.p@hotmail.es>
2023-09-06 09:01:49 +0200

    user repository backend

--- src/main/java/com/moviesnight/MovieNight/Repository/UserRepository.java
+++ src/main/java/com/moviesnight/MovieNight/Repository/UserRepository.java
@@ -1,5 +1,11 @@
 package com.moviesnight.MovieNight.Repository;
 
-public class UserRepository  {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.moviesnight.MoviesNight.Models.user;
+
+@Repository
+public interface UserRepository extends JpaRepository<user, Long>  {
    
 }
