399efc317f61561166f5dcc9d4fc88c47d5f6588
Ana <anarodriguez.p@hotmail.es>
2023-09-06 09:26:46 +0200

    service done

---
+++ src/main/java/com/moviesnight/MovieNight/Service/userService.java
@@ -0,0 +1,12 @@
+package com.moviesnight.MovieNight.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class userService {
+    
+         public static void main(String[] args) {
+            SpringApplication.run(userService.class, args);
+        }
+}
