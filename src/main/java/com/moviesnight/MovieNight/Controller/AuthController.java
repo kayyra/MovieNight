d643ff4c6a75c93f283031a45e404c31f5631772
Ana <anarodriguez.p@hotmail.es>
2023-09-07 08:58:29 +0200

    added all backend

--- src/main/java/com/moviesnight/MovieNight/Controller/AuthController.java
+++ src/main/java/com/moviesnight/MovieNight/Controller/AuthController.java
@@ -1,36 +1,46 @@
 package com.moviesnight.MovieNight.Controller;
 
-// import java.util.Map;
+import java.util.Optional;
+import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.apache.catalina.User;
 
-// import com.moviesnight.MovieNight.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// @RequestMapping("/api/auth")
+@RestController
+@RequestMapping(path = "api/v1/users")
 public class AuthController {
 
-    // @Autowired
-    // private UserRepository userRepository;
+    @Autowired
+    private UserService userService;
 
-    // @PostMapping("/login")
-    // public ResponseEntity<String> login(@RequestBody Map<String, String> credentials) {
-    //     String email = credentials.get("email");
-    //     String password = credentials.get("password");
+    @GetMapping("/{userId}")
+    public Optional<User> getBId(@PathVariable("userId") Long userID){
+        return userService.getUser(userID);
+    }
 
-    //     User user = userRepository.findById("id");
-    //     if (user != null && user.getPassword().equals(password)) {
-            
-    //         return ResponseEntity.ok("Login exitoso");
-    //     } else {
-    //         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales incorrectas");
-    //     }
+   
+    // @GetMapping
+    // public List<User> getAllUsers(){
+    //     return userService.getUsers();
     // }
+
+    @PostMapping
+    public User saveUpdate(@RequestBody User user){
+        userService.saveOrUpdate(user);
+        return user;
+    }
+
+    @DeleteMapping("/{userId}")
+    public void saveUpdate(@PathVariable("studentId") Long userId){
+        userService.delete(userId);
+    }
+    
 }
---
+++ src/main/java/com/moviesnight/MovieNight/Controller/UserService.java
@@ -0,0 +1,24 @@
+package com.moviesnight.MovieNight.Controller;
+
+import java.util.Optional;
+
+import org.apache.catalina.User;
+import org.hibernate.mapping.List;
+
+public class UserService {
+
+    public Optional<User> getUser(Long userID) {
+        return null;
+    }
+
+    public List getUsers() {
+        return null;
+    }
+
+    public void saveOrUpdate(User user) {
+    }
+
+    public void delete(Long userId) {
+    }
+
+}
