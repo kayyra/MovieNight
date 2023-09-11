36836dc68c2869e1005a73075dc3c927c642700e
Ana <anarodriguez.p@hotmail.es>
2023-09-11 10:02:46 +0200

    added card functions

--- src/main/frontend/src/app/views/play/play.component.ts
+++ src/main/frontend/src/app/views/play/play.component.ts
@@ -11,48 +11,71 @@ export class PlayComponent {
 
 
   movies: Movie[] = [
-    { id: 1, name: 'Lilo & Stitch', coverImage: 'assets/img/lilo&stitch.jpg',description: 'Description 1', isRevealed: false, flipped: false  },
+    { id: 1, name: 'Liloystitch', coverImage: 'assets/img/lilo&stitch.jpg',description: 'Description 1', isRevealed: false, flipped: false  },
     { id: 2, name: 'Free Guy', coverImage: 'assets/img/freeguy.jpg',description: 'Description 1', isRevealed: false, flipped: false },
     { id: 3, name: 'Barbie', coverImage: 'assets/img/barbie.jpg',description: 'Description 1', isRevealed: false, flipped: false },
-    { id: 4, name: 'Dungeons & Dragons', coverImage: 'assets/img/dungeons&dragons.jpg',description: 'Description 1', isRevealed: false, flipped: false  },   
+    { id: 4, name: 'Dungeonsanddragons', coverImage: 'assets/img/dungeons&dragons.jpg',description: 'Description 1', isRevealed: false, flipped: false  },   
     { id: 5, name: 'Intime', coverImage: 'assets/img/intime.jpg',description: 'Description 1', isRevealed: false, flipped: false  },
     { id: 6, name: 'HarryPotter', coverImage: 'assets/img/harrypotter1.jpg',description: 'Description 1', isRevealed: false, flipped: false }
   ];
-  
+
   shuffledCards: any[] = [];
+  selectedCard: Movie | null = null;
 
   constructor(private router: Router) {}
 
-  onCardClicked(movie: Movie) {
-    if (!movie.isRevealed) {
-      movie.isRevealed = true;
+  ngOnInit(){ 
+    this.shuffleCards();
+  } 
+   
+
+  onCardClick(card: Movie) {
+    if (!card.isRevealed){
+      card.isRevealed = true;
+      console.log(`Card ${card.name} is revealed: ${card.isRevealed}`);
+      this.selectedCard = card;
+      this.movies.forEach(movie =>{
+        if (movie !== card){
+          movie.isRevealed = false;
+        }
+      });
+      setTimeout(() => {
+        this.navigateToSelectedCard();
+      }, 2000);
     }
   }
 
-  ngOnInit(){  
-    this.shuffledCards = [...this.movies];
+  navigateToSelectedCard(){
+    console.log('Navigate to selected card');
+    if(this.selectedCard){
+      const route = `/${this.selectedCard.name.toLowerCase().replace(/\s+/g, '')}`;
+      this.router.navigate([route]);
+    }
   }
+
+  // onCardClicked(movie: Movie) {
+  //   if (!movie.isRevealed) {
+  //     movie.isRevealed = true;
+  //   }
+  // }
 
   shuffleCards() {
     this.shuffledCards = [...this.movies];
-    for (let i = this.shuffledCards.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
+    for(let i = this.shuffleCards.length - 1; i > 0; i--){
+      const j = Math.floor(Math.random() * ( i + 1));
       [this.shuffledCards[i], this.shuffledCards[j]] = [this.shuffledCards[j], this.shuffledCards[i]];
     }
+
+    this.movies.forEach(movie => (movie.isRevealed = false));
+  }
+    // this.shuffledCards = [...this.movies];
+    // for (let i = this.shuffledCards.length - 1; i > 0; i--) {
+    //   const j = Math.floor(Math.random() * (i + 1));
+    //   [this.shuffledCards[i], this.shuffledCards[j]] = [this.shuffledCards[j], this.shuffledCards[i]];
+    // }
     
-    this.movies.forEach(movie => (movie.flipped = false));
+    // this.movies.forEach(movie => (movie.flipped = false));
   }
 
-  onCardClick(card: any) {
-    card.flipped = true;
-    if (card.title === 'Lilo & Stitch') {
-      this.router.navigate(['/liloystitch']);
-    } else if (card.title === 'Free Guy') {
-      this.router.navigate(['/freeguy']);
-    } else if (card.title === 'Barbie') {
-      this.router.navigate(['/barbie']);
-    } else if (card.title === 'Dungeons & Dragons') {
-      this.router.navigate(['/dungeonsandragons']);
-    }
-  }
-}
+  
+
