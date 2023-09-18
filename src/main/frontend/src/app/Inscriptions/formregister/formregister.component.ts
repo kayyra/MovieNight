0ff604ec7aed0068cef0d3a4845635e3f45cc80c
Ana <anarodriguez.p@hotmail.es>
2023-09-18 09:56:21 +0200

    added register configuration

--- src/main/frontend/src/app/Inscriptions/formregister/formregister.component.ts
+++ src/main/frontend/src/app/Inscriptions/formregister/formregister.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-formregister',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./formregister.component.scss']
 })
 export class FormregisterComponent {
+formData: FormGroup;
+  http: any;
 
+constructor(private fb: FormBuilder){
+  this.formData = this.fb.group({    
+    username: [Validators.required],
+    password: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9])(?=.*[a-z]).{8,}$/)]]    
+  });
+}
+
+onSubmit(): void {
+  if(this.formData.valid){
+    const registrationData = this.formData.value;
+
+    this.http.post('/api/register', registrationData).subscribe(
+      (response: any) =>{
+        console.log('Register successfully:', Response );
+      },
+      (error: any) =>{
+        console.error('Error during registration:', error);
+      }
+    );
+  }
+}
 }
